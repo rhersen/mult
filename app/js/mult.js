ddc77ab59470a7024f9fcfd5ed200c9f86148a26
Rudolf Hersen <rhersen@gmail.com>
2013-09-13 17:04:22 +0200

    initial commit

---
+++ app/js/mult.js
@@ -0,0 +1,80 @@
+var helloText = function () {
+    return 'Hello, World!';
+};
+
+function getPairs(n) {
+    var r = [];
+
+    for (var x = 2; x < n; x++) {
+        for (var y = 2; y <= x; y++) {
+            r.push({x: x, y: y});
+        }
+    }
+
+    return r;
+}
+var init = function () {
+    function next() {
+        ++p;
+    }
+
+    function x() {
+        return pairs[p].x;
+    }
+
+    function y() {
+        return pairs[p].y;
+    }
+
+    function isCorrect(answer) {
+        return answer === '' + (x() * y());
+    }
+
+    function updateQuestion() {
+        $('.factor-x').text(x());
+        $('.factor-y').text(y());
+        $('#answer').val('');
+    }
+
+    function showAnswer() {
+        function show(row, col) {
+            $('td.row' + row + '.col' + col).removeClass('remaining');
+        }
+
+        show(x(), y());
+        show(y(), x());
+    }
+
+    function checkAnswer() {
+        if (isCorrect($(this).val())) {
+            showAnswer();
+            next();
+            updateQuestion();
+        }
+    }
+
+    var limit = 5;
+    var pairs = _.shuffle(getPairs(limit));
+    var p = 0;
+
+    $('body').append(JST['app/templates/hello.us']({
+        x: 0, y: 0
+    }));
+
+    for (var i = 1; i < limit; i++) {
+        var row = $('<tr></tr>').appendTo('table');
+
+        for (var j = 1; j < limit; j++) {
+            $('<td>' + (i * j) + '</td>')
+                .addClass('row' + i)
+                .addClass('col' + j)
+                .appendTo(row);
+        }
+    }
+
+    $('td').addClass('remaining');
+    $('td.row1').removeClass('remaining');
+    $('td.col1').removeClass('remaining');
+    $("#answer").keyup(checkAnswer);
+    updateQuestion();
+};
